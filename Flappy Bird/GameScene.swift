791a2c3180afabf0faac029d65d9b3a513cf6ac4
Tobias Brysiewicz <tbrysiewicz@att.net>
2015-12-22 13:28:16 -0800

    Added Collisions

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -8,13 +8,25 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var bird = SKSpriteNode()
     var bg = SKSpriteNode()
     
+    let birdGroup:UInt32 = 1
+    let objectGroup:UInt32 = 2
+    
+    var gameOver = 0
+    
+    var movingObjects = SKNode()
+    
     override func didMoveToView(view: SKView) {
         
+        self.physicsWorld.contactDelegate = self
+        
+        self.physicsWorld.gravity = CGVectorMake(0, -5)
+        
+        self.addChild(movingObjects)
         
         // BACKGROUDN ANIMATION
         // --------------
@@ -32,11 +44,9 @@ class GameScene: SKScene {
             
             bg.runAction(movebgForever)
             
-            self.addChild(bg)
+            movingObjects.addChild(bg)
             
         }
-        
-
         
         
         // BIRD ANIMATION
@@ -51,15 +61,102 @@ class GameScene: SKScene {
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         bird.runAction(makeBirdFlap)
         
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        bird.physicsBody?.categoryBitMask = birdGroup
+        bird.physicsBody?.collisionBitMask = objectGroup
+        bird.physicsBody?.contactTestBitMask = objectGroup
+        
+        bird.zPosition = 10
         self.addChild(bird)
+        
+        
+        // GROUDN CODE
+        // -----------
+        let ground = SKNode()
+        ground.position = CGPointMake(0, 0)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody?.dynamic = false
+        ground.physicsBody?.categoryBitMask = objectGroup
+        
+        self.addChild(ground)
+        
+        
+        // PIPE TIMER
+        // -----------
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("makePipes"), userInfo: nil, repeats: true)
+
+    }
+    
+    
+    func makePipes() {
+
+        if (gameOver == 0) {
+        
+        let gapHeight = bird.size.height * 4
+        
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        
+        let movePipes = SKAction.moveByX(-self.frame.size.width * 2, y: 0, duration: NSTimeInterval(self.frame.size.width / 100))
+        let removePipes = SKAction.removeFromParent()
+        let moveAndRemovePipes = SKAction.sequence([movePipes, removePipes])
+        
+        
+        
+        
+        
+        let pipe1Texture = SKTexture(imageNamed: "pipe1.png")
+        let pipe1 = SKSpriteNode(texture: pipe1Texture)
+        pipe1.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + pipe1.size.height / 2 + gapHeight / 2 + pipeOffset)
+        
+        pipe1.runAction(movePipes)
+        
+        pipe1.physicsBody = SKPhysicsBody(rectangleOfSize: pipe1.size)
+        pipe1.physicsBody?.dynamic = false
+        pipe1.physicsBody?.categoryBitMask = objectGroup
+        
+        movingObjects.addChild(pipe1)
+        
+        let pipe2Texture = SKTexture(imageNamed: "pipe2.png")
+        let pipe2 = SKSpriteNode(texture: pipe2Texture)
+        pipe2.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - pipe2.size.height / 2 - gapHeight / 2 + pipeOffset)
+        
+        pipe2.runAction(movePipes)
+        
+        pipe2.physicsBody = SKPhysicsBody(rectangleOfSize: pipe2.size)
+        pipe2.physicsBody?.dynamic = false
+        pipe2.physicsBody?.categoryBitMask = objectGroup
+        
+        movingObjects.addChild(pipe2)
+            
+        }
         
     }
     
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        gameOver = 1
+        
+        movingObjects.speed = 0
+    }
+    
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        
-    
+        if (gameOver == 0) {
+        
+        bird.physicsBody?.velocity = CGVectorMake(0, 0)
+        bird.physicsBody?.applyImpulse(CGVectorMake(0, 50))
+        
+        }
+        
     }
    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
