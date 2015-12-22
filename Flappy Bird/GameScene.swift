c9f826928f1994eef0e9dba6e63240fc359bdcec
Tobias Brysiewicz <tbrysiewicz@att.net>
2015-12-22 10:10:36 -0800

    Added Infinite Background

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -11,15 +11,41 @@ import SpriteKit
 class GameScene: SKScene {
     
     var bird = SKSpriteNode()
-    
+    var bg = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
         
-        var birdTexture = SKTexture(imageNamed: "flappy1.png")
-        var birdTexture2 = SKTexture(imageNamed: "flappy2.png")
         
-        var animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
-        var makeBirdFlap = SKAction.repeatActionForever(animation)
+        // BACKGROUDN ANIMATION
+        // --------------
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        let movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        let replacebg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        let movebgForever = SKAction.repeatActionForever(SKAction.sequence([movebg, replacebg]))
+        
+        for var i:CGFloat=0; i<3; i++ {
+            
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width/2 + bgTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            bg.size.height = self.frame.height
+            
+            bg.runAction(movebgForever)
+            
+            self.addChild(bg)
+            
+        }
+        
+
+        
+        
+        // BIRD ANIMATION
+        // --------------
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        
+        let animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
         
         bird = SKSpriteNode(texture: birdTexture)
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
